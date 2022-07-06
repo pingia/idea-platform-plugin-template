cf5cae57fe71952c40f98bf132759d280f7391c8
GitHub Action <action@github.com>
2022-07-06 07:09:20 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/pingia/ideaplatformplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.pingia.ideaplatformplugintemplate.services
+
+import com.github.pingia.ideaplatformplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/pingia/ideaplatformplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/pingia/ideaplatformplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.pingia.ideaplatformplugintemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pingia.ideaplatformplugintemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
